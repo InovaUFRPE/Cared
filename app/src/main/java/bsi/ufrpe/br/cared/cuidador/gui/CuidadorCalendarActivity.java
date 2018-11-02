7430736d917ef2b57a465a90e47615da48ec0196
ThiagoCMS <ThiagoCMS@users.noreply.github.com>
2018-11-02 18:24:07 -0300

    Calendario pegar agendamentos (incompleto)

--- app/src/main/java/bsi/ufrpe/br/cared/cuidador/gui/CuidadorCalendarActivity.java
+++ app/src/main/java/bsi/ufrpe/br/cared/cuidador/gui/CuidadorCalendarActivity.java
@@ -7,23 +7,43 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.DayViewFacade;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import bsi.ufrpe.br.cared.R;
+import bsi.ufrpe.br.cared.horario.dominio.Agendamento;
 import bsi.ufrpe.br.cared.infra.EventDecorator;
+import bsi.ufrpe.br.cared.infra.Sessao;
 
 public class CuidadorCalendarActivity extends AppCompatActivity {
     private MaterialCalendarView mcv;
     private EventDecorator eventDecorator;
+    private List<CalendarDay> calendarDayList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cuidador_calendar);
         mcv = findViewById(R.id.calerdarViewId);
+        eventDecorator = new EventDecorator(0, calendarDayList) {
+            @Override
+            public boolean shouldDecorate(CalendarDay day) {
+                return super.shouldDecorate(day);
+            }
+
+            @Override
+            public void decorate(DayViewFacade view) {
+                super.decorate(view);
+            }
+        };
         mcv.setOnDateChangedListener(new OnDateSelectedListener() {
             @Override
             public void onDateSelected(@NonNull MaterialCalendarView materialCalendarView, @NonNull CalendarDay calendarDay, boolean b) {
@@ -45,5 +65,25 @@ public class CuidadorCalendarActivity extends AppCompatActivity {
             startActivity(new Intent(CuidadorCalendarActivity.this, CadastrarHorarioDisponivelActivity.class));
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void getHorarios(){
+        Sessao.getDatabaseAgendamento().addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for(DataSnapshot dataSnapshot1: dataSnapshot.getChildren()){
+                    Agendamento agendamento = dataSnapshot1.getValue(Agendamento.class);
+                    if(agendamento.getCuidadorId().equals(Sessao.getUserId())){
+
+                    }
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/bsi/ufrpe/br/cared/infra/servico/CalendarTypeConverter.java
@@ -0,0 +1,17 @@
+package bsi.ufrpe.br.cared.infra.servico;
+
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import bsi.ufrpe.br.cared.horario.dominio.Horario;
+
+public class CalendarTypeConverter {
+
+    public static List<CalendarDay> toCalendarDay(Horario horario){
+        List<CalendarDay> list = new ArrayList<>();
+        
+        return list;
+    }
+}
